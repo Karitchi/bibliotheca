46e9522aaaf2c27252ad08c210724053c9cab926
karitchi <karichikarichikarichi@gmail.com>
2024-05-06 12:56:19 +0200

    feat: Organize project structure by moving Node.js files to src directory
    Moved node.js code (app.js, public/sign-up.html, routes/signUp.js, utils/db.js, utils/publicDirectoryPath.js and utils/signUp.js) to src directory

--- src/app.js
+++ src/app.js
@@ -1,13 +1,18 @@
 import express from "express";
 import signUpRouter from "./routes/signUp.js";
-import connectToDB from "./utils/DBConnection.js";
+import * as db from "./utils/db.js";
 
-await connectToDB();
+try {
+  const NOW = await db.query("SELECT NOW()");
+  console.log("Connected to db", NOW.rows[0].now);
+} catch (error) {
+  console.error("Error connecting db", error);
+}
 
 const app = express();
 
 const port = process.env.NODE_PORT || 3000;
-const host = process.env.NODE_HOST || "127.0.0.1";
+const host = process.env.NODE_HOST || "0.0.0.0";
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
